e75e381a1bb3fab42c59ced72fbc5372fae8d85b
Niel <nielmce@gmail.com>
2016-10-07 15:08:08 +0100

    first commit

--- Boggle.py
+++ Boggle.py
@@ -1,9 +1,17 @@
-from string import ascii_uppercase
 from random import choice
-def make_grid(width, height):
-    return {(row,col): choice(ascii_uppercase)
-            for row in range(height)
-            for col in range(width)}
+
+def make_grid(height, width):
+    dice = ['CSOAHP', 'ABBOJO', 'ENSIUE', 'FSKFAP','EGAENE', 'YIDSTT', 'ATTOWO',
+'TOESIS', 'RHTVWE', 'HLNNRZ', 'MTIOUC', 'NEEHGW', 'DIXRLE', 'YLDERV',
+'QHMUNI', 'RETTYL']
+
+
+
+
+    return {(row,col): choice(choice(dice))
+                  for row in range (height)
+                  for col in range(width)}
+
 
 def neighbours_of_position((row,col)):
     return [ (row -1, col -1), (row - 1, col), (row - 1, col + 1),
@@ -16,3 +24,93 @@ def all_grid_neighbours(grid):
         position_neighbours = neighbours_of_position(position)
         neighbours[position] = [p for p in position_neighbours if p in grid]
     return neighbours
+
+def pos_to_letter(grid, p):
+    return 'QU' if grid[p] == 'Q' else grid[p]
+
+    # if grid[pos] == 'Q':
+    #     return 'QU'
+    # else:
+    #     return grid[pos]
+
+
+def path_to_word(grid, path):
+    return ''.join([pos_to_letter(grid, p) for p in path])
+
+def is_a_real_word(word, dictionary):
+    return word in dictionary
+
+def search(grid, dictionary):
+    neighbours = all_grid_neighbours(grid)
+    paths = []
+    full_words, stems = dictionary
+
+    def do_search(path):
+        word = path_to_word(grid, path)
+        if is_a_real_word(word, full_words):
+            paths.append(path)
+        if word not in stems:
+            return
+        for next_pos in neighbours[path[-1]]:
+            if next_pos not in path:
+                do_search(path + [next_pos])
+
+    for position in grid:
+
+        do_search([position])
+
+    words = []
+
+    for path in paths:
+        words.append(path_to_word(grid, path))
+    return set(words)
+
+def get_dictionary(dictionary_file):
+    full_words, stems = set(), set()
+
+    with open(dictionary_file) as f:
+        for word in f:
+            word = word.strip().upper()
+            full_words.add(word)
+
+            for i in range(1, len(word)):
+                stems.add(word[:i])
+
+        return full_words, stems
+
+def display_grid (grid):
+    height, width = max(grid)
+    height +=1
+    width +=1
+
+
+    for r in range(height):
+        rowAsText = " ".join(grid[(r,c)] for c in range(width))
+        print rowAsText
+
+
+
+
+
+
+def display_words(words):
+    for word in words:
+        print word
+    print ", ".join(words)
+    longest = max(words, key=lambda w: len(w))
+    print "Found {0} words".format(len(words))
+    print longest + " is the longest word found"
+
+def check():
+ return 1
+
+def main():
+    grid = make_grid(4,4)
+    dictionary = get_dictionary('bogwords.txt')
+    words = search(grid, dictionary)
+    display_words(words)
+    display_grid(grid)
+
+
+
+main()
--- test_boggle.py
+++ test_boggle.py
@@ -45,3 +45,33 @@ class TestBoggle(unittest.TestCase):
             others[:] = grid
             others.remove(pos)
             self.assertListEqual(sorted(neighbours[pos]), sorted(others))
+
+    def test_converting_a_path_to_a_word(self):
+        grid = Boggle.make_grid(2,2)
+        oneLetterWord = Boggle.path_to_word(grid, [(0,0)])
+        twoLetterWord = Boggle.path_to_word(grid, [(0,0),(1,1)])
+        self.assertEqual(oneLetterWord, grid[(0,0)])
+        self.assertEqual(twoLetterWord, grid[(0,0)] + grid[(1,1)])
+
+    def test_search_grid_for_words(self):
+        grid = {(0, 0): 'A', (0, 1): 'B', (1, 0): 'C', (1, 1): 'D'}
+        twoLetterWord = 'AB'
+        threeLetterWord = 'ABC'
+        notThereWord = "EEE"
+
+        fullWords = [twoLetterWord, threeLetterWord, notThereWord]
+        stems = ['A', 'AB', 'E', 'EE']
+        dictionary = fullWords, stems
+
+        foundWords = Boggle.search(grid, dictionary)
+
+        self.assertTrue(twoLetterWord in foundWords)
+        self.assertTrue(threeLetterWord in foundWords)
+        self.assertTrue(notThereWord not in foundWords)
+
+    def test_load_dictionary(self):
+        dictionary = Boggle.get_dictionary('bogwords.txt')
+        self.assertGreater(len(dictionary), 0)
+
+    def test_is_this_thing_on(self):
+        self.assertEquals(1, Boggle.check())
